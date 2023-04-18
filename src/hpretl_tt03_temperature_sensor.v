25d35b996851f8c93cc50aa8b1bbfd92b9596fa8
Harald Pretl <harald@pretl.eu>
2023-04-18 20:39:49 +0200

    Remove calibration engine, design too large

--- src/hpretl_tt03_temperature_sensor.v
+++ src/hpretl_tt03_temperature_sensor.v
@@ -13,7 +13,7 @@
 //	limitations under the License.
 
 //	TODO
-//	- DONE Add LUT for result calibration
+//	- CANCEL (too large) Add LUT for result calibration
 //	- DONE Add more testmodes
 //	- Simulate logic
 //	- Simulate mixed-signal
@@ -44,11 +44,7 @@ module hpretl_tt03_temperature_sensor (
 	// definition of external inputs
 	wire clk = io_in[0];
 	wire reset = io_in[1];
-	wire cal_clk = io_in[2];
-	wire cal_dat = io_in[3];
-	wire cal_ld = io_in[4];
-	wire cal_ena = io_in[5];
-	wire [1:0] en_dbg = io_in[7:6];
+	wire [2:0] en_dbg = io_in[7:5];
 
 
 	// definition of external outputs
@@ -60,11 +56,7 @@ module hpretl_tt03_temperature_sensor (
 	// definition of internal wires and regs
 	reg [N_CTR-1:0] ctr;
 	reg [N_VDAC-1:0] tempsens_res_raw;
-	reg [N_VDAC-1:0] calib_mem[0:2**(N_VDAC-1)-1];
-	reg [N_VDAC-1:0] calib_incoming_word;
-	reg [N_VDAC-2:0] calib_store_ctr;
 
-	wire [N_VDAC-1:0] tempsens_res;
 	wire [1:0] meas_state = ctr[1:0];
 	wire [N_VDAC-1:0] dac_value = ctr[N_VDAC+1:2];
 	wire show_tens = ~ctr[N_CTR-1];
@@ -78,9 +70,11 @@ module hpretl_tt03_temperature_sensor (
 
 
 	// debug vectors
-	wire [7:0] dbg1 = cal_ena ? {tempsens_en, tempsens_measure, tempsens_dat} : {temp_delay, in_reset, in_precharge, in_transition, in_transition_ph0, in_transition_ph1, in_measurement, in_evaluation};
-	wire [7:0] dbg2 = cal_ena ? {ctr[7:0]} : {meas_state, tempsens_res_raw};
-	wire [7:0] dbg3 = cal_ena ? {{(N_CTR-12){1'b0}}, ctr[N_CTR-1:8]} : {show_ones, show_tens, cal_ena, calib_store_ctr};
+	wire [7:0] dbg1 = {temp_delay, in_reset, in_precharge, in_transition, in_transition_ph0, in_transition_ph1, in_measurement, in_evaluation};
+	wire [7:0] dbg2 = {tempsens_en, tempsens_measure, tempsens_dat}; 
+	wire [7:0] dbg3 = {meas_state, tempsens_res_raw};
+	wire [7:0] dbg4 = {ctr[7:0]};
+	wire [7:0] dbg5 = {show_ones, show_tens, ctr[N_CTR-1:8]};
 
 
 	// measurement state machine (meas_state)
@@ -93,10 +87,6 @@ module hpretl_tt03_temperature_sensor (
 	// VDAC max and min value
 	localparam VMAX = {N_VDAC{1'b1}};
 	localparam VMIN = {N_VDAC{1'b0}};
-
-
-	// select raw or calibrated result
-	assign tempsens_res = cal_ena ? calib_mem[tempsens_res_raw[N_VDAC-2:0]] : tempsens_res_raw;
 
 
 	// create state signals based on state of state machine
@@ -126,10 +116,12 @@ module hpretl_tt03_temperature_sensor (
 
 
 	// display decimal number (tens or ones) on number LED or debug signals
-	assign led_out = 	(en_dbg == 2'b00) ? {show_ones, segments} :
-						(en_dbg == 2'b01) ? dbg1 :
-						(en_dbg == 2'b10) ? dbg2 :
-						(en_dbg == 2'b11) ? dbg3 :
+	assign led_out = 	(en_dbg == 3'd0) ? {show_ones, segments} :
+						(en_dbg == 3'd1) ? dbg1 :
+						(en_dbg == 3'd2) ? dbg2 :
+						(en_dbg == 3'd3) ? dbg3 :
+						(en_dbg == 3'd4) ? dbg4 :
+						(en_dbg == 3'd5) ? dbg5 :
 						{show_ones, segments};
 	
 
@@ -150,27 +142,6 @@ module hpretl_tt03_temperature_sensor (
 	end
 
 
-	// state machine for calibration memory loading
-	// the new word is first loaded serially (we just keep shifting as long as clk pulsed come)
-	always @(posedge cal_clk) begin
-		if (reset) begin
-			calib_incoming_word <= {N_VDAC{1'b0}};
-		end else begin
-			calib_incoming_word <= {calib_incoming_word[N_VDAC-2:0],cal_dat};
-		end
-	end
-	// the incoming word is then stored into the calibration memory, the store counter just
-	// keeps rolling around (but starts at zero after reset)
-	always @(posedge cal_ld) begin
-		if (reset) begin
-			calib_store_ctr <= {(N_VDAC-1){1'b0}};
-		end else begin
-			calib_mem[calib_store_ctr] <= calib_incoming_word;
-			calib_store_ctr <= calib_store_ctr + 1'b1;
-		end
-	end
-
-
     // instantiate temperature-dependent delay (this is the core circuit)
     tempsense #(.DAC_RESOLUTION(N_VDAC), .CAP_LOAD(16)) temp1 (
         .i_dac_data(tempsens_dat),
@@ -182,7 +153,7 @@ module hpretl_tt03_temperature_sensor (
 
 	// binary to decimal decoder to show measurement result on 7-segment LED
 	bin2dec dec1 (
-		.i_bin(tempsens_res),
+		.i_bin(tempsens_res_raw),
 		.i_tens(show_tens),
 		.i_ones(show_ones),
 		.o_dec(digit)
